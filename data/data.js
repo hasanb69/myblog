56d895685ac33723049eb11bbeca38368bd6c4b1
unknown <hasanbaratiasp@gmail.com>
2021-05-31 17:48:28 +0430

    install bootstrap

---
+++ data/data.js
@@ -0,0 +1,63 @@
+const DATA={
+    honar:{
+       title:'honar',
+       id:1,
+       items:[
+          {
+              id:1,
+              childtitle:'the artice of ?',
+              discription:'Get started with Bootstrap, the world’s most popular framework for building responsive, mobile-first sites, with jsDelivr and a template starter page.',
+              imgeurl:'',
+              category:'honar'
+          },
+          {
+            id:2,
+            childtitle:'the artice of ?',
+            discription:'Get started with Bootstrap, the world’s most popular framework for building responsive, mobile-first sites, with jsDelivr and a template starter page.',
+            imgeurl:'',
+            category:'honar'
+        },
+        {
+            id:3,
+            childtitle:'the artice of ?',
+            discription:'Get started with Bootstrap, the world’s most popular framework for building responsive, mobile-first sites, with jsDelivr and a template starter page.',
+            imgeurl:'',
+            category:'honar'
+        }
+       ]    
+    },
+
+    sport:{
+        title:'sport',
+        id:2,
+        items:[
+           {
+               id:1,
+               childtitle:'the artice of ?',
+               discription:'Get started with Bootstrap, the world’s most popular framework for building responsive, mobile-first sites, with jsDelivr and a template starter page.',
+               imgeurl:'',
+               category:'sport'
+           },
+           {
+             id:2,
+             childtitle:'the artice of ?',
+             discription:'Get started with Bootstrap, the world’s most popular framework for building responsive, mobile-first sites, with jsDelivr and a template starter page.',
+             imgeurl:'',
+             category:'sport'
+         },
+         {
+             id:3,
+             childtitle:'the artice of ?',
+             discription:'Get started with Bootstrap, the world’s most popular framework for building responsive, mobile-first sites, with jsDelivr and a template starter page.',
+             imgeurl:'',
+             category:'sport'
+         }
+        ]    
+     }
+
+
+
+
+
+}
+  
